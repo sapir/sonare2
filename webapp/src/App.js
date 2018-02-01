ffd28453babdc5a5e611035d3ae66b6df62c96c1
Y. Sapir <yasapir@gmail.com>
2018-02-01 04:55:11 +0200

    Use semantic-ui-react.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import { Sidebar, Segment, Header, List } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Segment} animation='push' width='thin' visible={true} icon='labeled' inverted vertical>
+            <Header as="h3">Names</Header>
+            <List>
+              <List.Item>main</List.Item>
+              <List.Item>func1</List.Item>
+              <List.Item>func2</List.Item>
+            </List>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Segment basic>
+              disassembly goes here
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     );
   }
