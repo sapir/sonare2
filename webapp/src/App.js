85d8457d3d4ca3262de442a2c43b215c790f0ce0
Y. Sapir <yasapir@gmail.com>
2018-02-02 15:50:52 +0200

    Only color register operands as red, not numbers.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -31,8 +31,17 @@ class App extends Component {
     this.setState({func: json});
   }
 
-  renderAsmToken(token, i) {
-    const className = `token-${token.type}`;
+  renderAsmToken(asmLine, token, i) {
+    let className = `token-${token.type}`;
+
+    if (token.type === "operand") {
+      const op = asmLine.operands[token.index];
+      className += ` token-operand-${op.type}`;
+
+      if (token.part_type !== "full") {
+        className += ` token-operand-${op.type}-${token.part_type}`;
+      }
+    }
 
     return (
       <span key={i} className={className}>
@@ -44,7 +53,7 @@ class App extends Component {
   renderAsmLine(asmLine) {
     return (
       <div key={asmLine.start}>
-        {_.map(asmLine.tokens, (token, i) => this.renderAsmToken(token, i))}
+        {_.map(asmLine.tokens, (token, i) => this.renderAsmToken(asmLine, token, i))}
       </div>
     );
   }
