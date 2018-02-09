ddb41ab376f2b593d7c599f945b10a61a4dad9a5
Y. Sapir <yasapir@gmail.com>
2018-02-09 13:00:37 +0200

    Wrap App in a HashRouter.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <HashRouter>
+        <Route component={App} />
+    </HashRouter>,
+    document.getElementById('root'));
+
 registerServiceWorker();
