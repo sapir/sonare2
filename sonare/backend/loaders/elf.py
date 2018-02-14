790bdad87473b6ea1aa65b1296772e5eef4fa38b
Y. Sapir <yasapir@gmail.com>
2018-02-15 01:21:49 +0200

    Load ELF sections instead of segments when missing (i.e. relocatable files).

--- sonare/backend/loaders/elf.py
+++ sonare/backend/loaders/elf.py
@@ -26,7 +26,11 @@ class Elf:
             arch = backend.get_arch()
 
             # TODO: use sections if no segments
-            self._load_segments_as_sections(backend)
+            segments = self._get_segments()
+            if segments:
+                self._load_segments_as_sections(backend, segments)
+            else:
+                self._load_sections_as_sections(backend, self._get_sections())
 
             for sym in self.iter_symbols():
                 if not sym.name:
@@ -67,21 +71,27 @@ class Elf:
 
             arch.hook_post_load_file(self.fileobj.name, "elf")
 
-    def _load_segments_as_sections(self, backend):
-        for segment in self.elffile.iter_segments():
-            if segment["p_type"] == "PT_LOAD":
-                addr = segment["p_vaddr"]
-                data = segment.data()
-                backend.sections.add(addr, data)
+    def _get_segments(self):
+        return [
+            (segment["p_vaddr"], segment.data())
+            for segment in self.elffile.iter_segments()
+            if segment["p_type"] == "PT_LOAD"
+        ]
 
-    def _load_sections_as_sections(self, backend):
-        for section in self.elffile.iter_sections():
-            if section["sh_flags"] & SH_FLAGS.SHF_ALLOC:
-                name = section.name
-                addr = section["sh_addr"]
-                data = section.data()
+    def _get_sections(self):
+        return [
+            (section["sh_addr"], section.data(), section.name)
+            for section in self.elffile.iter_sections()
+            if section["sh_flags"] & SH_FLAGS.SHF_ALLOC
+        ]
 
-                backend.sections.add(addr, data, name=name)
+    def _load_segments_as_sections(self, backend, segments):
+        for addr, data in segments:
+            backend.sections.add(addr, data)
+
+    def _load_sections_as_sections(self, backend, sections):
+        for addr, data, name in sections:
+            backend.sections.add(addr, data, name=name)
 
     def iter_symbols(self):
         for section in self.elffile.iter_sections():
